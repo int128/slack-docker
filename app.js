9c55241fabcbc8ce4c812ca7f108d6fd3aa28b32
int128 <int128@gmail.com>
2015-12-31 19:27:16 +0900

    Refactor with pipes

--- app.js
+++ app.js
@@ -4,79 +4,97 @@ const Promise     = require('bluebird');
 const JSONStream  = require('JSONStream');
 const EventStream = require('event-stream');
 const Dockerode   = require('dockerode');
-const SlackNotify = require('slack-notify');
+const Slack       = require('slack-notify');
 
-const Docker = new Dockerode();
-Promise.promisifyAll(Docker);
-
-const Slack = SlackNotify(process.env.webhook);
-Promise.promisifyAll(Slack);
-
-const EventFilter = {
-  _imageRegExp: new RegExp(process.env.image_regexp),
-  satisfy(event) {
-    return this._imageRegExp.test(event.from);
+class EventFilter {
+  constructor(image) {
+    this._imageRegExp = new RegExp(image);
   }
-};
+  filter() {
+    return EventStream.map((event, callback) => {
+      if (this._imageRegExp.test(event.from)) {
+        callback(null, event);
+      } else {
+        callback();
+      }
+    });
+  }
+}
 
-class EventProcessor {
-  constructor() {
+class EventInspector {
+  constructor(docker) {
+    this._docker = docker;
     this._containers = {};
   }
-  _format(container, text, fields) {
-    return {
-      username: `docker${container.Name}`,
+  map() {
+    return EventStream.map((event, callback) => {
+      if (this._containers[event.id]) {
+        event.container = this._containers[event.id];
+        callback(null, event);
+      } else {
+        Promise.promisifyAll(this._docker.getContainer(event.id))
+        .inspectAsync()
+        .then((container) => {
+          event.container = this._containers[event.id] = container;
+          callback(null, event);
+        });
+      }
+      if (event.status == 'destroy') {
+        delete this._containers[event.id];
+      }
+    });
+  }
+}
+
+class EventNotifier {
+  constructor(slack) {
+    this._slack = slack;
+  }
+  _send(event, text, fields) {
+    return this._slack.sendAsync({
+      username: `docker${event.container.Name}`,
       icon_emoji: ':whale:',
       channel: '',
       text: text,
       fields: fields
-    };
-  }
-  handle(event) {
-    const method = this[`handle_${event.status}`];
-    if (method) {
-      return method.call(this, event);
-    }
-  }
-  handle_start(event) {
-    const containerObject = Docker.getContainer(event.id);
-    Promise.promisifyAll(containerObject);
-    containerObject.inspectAsync().then((container) => {
-      this._containers[event.id] = container;
-      return Slack.sendAsync(this._format(container, `Started ${container.Config.Hostname}`, {
-        'Image': event.from,
-        'IP Address': container.NetworkSettings.IPAddress,
-        'Path': container.Path,
-        'Arguments': container.Args,
-        'Started at': container.State.StartedAt
-      }));
     });
   }
-  handle_kill(event) {
-    const container = this._containers[event.id];
-    if (container) {
-      return Slack.sendAsync(this._format(container, `Stopped ${container.Config.Hostname}`));
-    }
+  _map_start(e) {
+		return this._send(e, `Started ${e.container.Config.Hostname}`, {
+      'Image': e.from,
+      'IP Address': e.container.NetworkSettings.IPAddress,
+      'Path': e.container.Path,
+      'Arguments': e.container.Args,
+      'Started at': e.container.State.StartedAt
+    });
   }
-  handle_die(event) {
-    return this.handle_kill(event);
-  }
-  handle_destroy(event) {
-    const container = this._containers[event.id];
-    if (container) {
-      delete this._containers[event.id];
-      return Slack.sendAsync(this._format(container, `Removed ${container.Config.Hostname}`));
-    }
+  _map_kill(e) {
+		return this._send(e, `Stopped ${e.container.Config.Hostname}`);
+	}
+  _map_die(e) {
+		return this._send(e, `Stopped ${e.container.Config.Hostname}`);
+	}
+  _map_destroy(e) {
+		return this._send(e, `Removed ${e.container.Config.Hostname}`);
+	}
+  map() {
+    return EventStream.map((event, callback) => {
+      if (this[`_map_${event.status}`]) {
+        this[`_map_${event.status}`](event).then((sent) => callback(null, sent));
+      }
+    });
   }
 }
 
-Docker.versionAsync()
+const docker = Promise.promisifyAll(new Dockerode());
+const slack  = Promise.promisifyAll(Slack(process.env.webhook));
+
+docker.versionAsync()
 .then((version) => console.info(version))
-.then(() => Docker.getEventsAsync())
-.then((stream) => {
-  const eventProcessor = new EventProcessor();
-  stream.pipe(JSONStream.parse())
-    .pipe(EventStream.map((event) => console.info(event)))
-    .pipe(EventStream.map((event) => eventProcessor.handle(event)))
-})
-.catch((e) => console.error(e));
+.then(() => docker.getEventsAsync())
+.then((stream) => stream
+  .pipe(JSONStream.parse())
+  .pipe(new EventFilter(process.env.image_regexp).filter())
+  .pipe(new EventInspector(docker).map())
+  .pipe(new EventNotifier(slack).map())
+).catch((e) => console.error(e));
